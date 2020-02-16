7acd31e56b9231447fad0a68f7b5d033e323128a
Zeeshan Syed <zsyed89@gmail.com>
2020-02-16 15:49:00 -0500

    Emit items based on a duration with interval and timer

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-import { of, from } from "rxjs";
+import { interval, timer } from 'rxjs';
 
-const observer = {
-    next: val => console.log('next', val),
-    error: err => console.log('error', err),
-    complete: () => console.log('complete!')
-};
+/*
+ * interval emits numbers in sequence based on the
+ * duration that you specify. In this case, a number
+ * will be emitted every 1000ms (1s)
+ */
+const interval$ = interval(1000);
 
-const source$ = from(fetch(
-    'https://api.github.com/users/syedz'
-));
+/*
+ * We'll just supply a function for next in this case,
+ * rather than observer object.
+ */
+interval$.subscribe(console.log);
 
-source$.subscribe(observer);
+/*
+ * If you need the first item to be emitted on an interval
+ * different than the rest, you can use the timer operator instead.
+ * For example, let's have the first item emit immediately, followed
+ * by a value every 1000ms after.
+ */
+// const timer$ = timer(0, 1000);
+
+/*
+ * You can also emit a single item after a specified duration, then complete,
+ * by just supplying the first argument.
+ */
+// const timer$ = timer(1000);
