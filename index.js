c76705b8e408c18ec43da0f8c56bd96a0238b97b
Zeeshan Syed <zsyed89@gmail.com>
2020-02-16 15:46:31 -0500

    Turn arrays, iterators, and promises into observables using from

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { of, range } from "rxjs";
+import { of, from } from "rxjs";
 
 const observer = {
     next: val => console.log('next', val),
@@ -6,23 +6,8 @@ const observer = {
     complete: () => console.log('complete!')
 };
 
-/*
- * Emits each item you provide in sequence, synchronously.
- * of literally just loops through the items and emits them,
- * there is no flattening involved. For instance, if you pass an
- * array the entire array will be emitted, not each item within
- * the array.
- */
-const source$ = of(1,2,3,4,5);
+const source$ = from(fetch(
+    'https://api.github.com/users/syedz'
+));
 
-console.log('proving');
 source$.subscribe(observer);
-console.log('this is synchronous');
-
-/*
- * If you just want to emit numbers between a specific range
- * you could also use the range operator instead.
- */
-console.log('proving');
-range(1,5).subscribe(observer);
-console.log('this is synchronous');
